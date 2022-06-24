464ba7ac6e8bcdc6421577a194d6aadca10993fc
Shin Jeong Min <sin37minduke@naver.com>
2022-06-24 23:31:26 +0900

    Add room change system

--- socket.io/socket.io.js
+++ socket.io/socket.io.js
@@ -13,26 +13,29 @@ app.get('/', function(req, res){
     res.sendFile(fileName, options);
 });
 
-io.on('connection', (socket) => {   //연결이 들어오면 실행되는 이벤트
-    // socket 변수에는 실행 시점에 연결한 상대와 연결된 소켓의 객체가 들어있다.
+io.on('connection', (socket) => {
+    socket.emit('usercount', io.engine.clientsCount); // usercount 호출 시 client 수를 보냄
 
-    //socket.emit으로 현재 연결한 상대에게 신호를 보낼 수 있다.
-    socket.emit('usercount', io.engine.clientsCount);
+    //시작시 default 룸에 입장
+    socket.join("채팅방 1");
 
-    // on 함수로 이벤트를 정의해 신호를 수신할 수 있다.
-    socket.on('message', (msg) => {
-        //msg에는 클라이언트에서 전송한 매개변수가 들어온다. 이러한 매개변수의 수에는 제한이 없다.
-        console.log('Message received: ' + msg);
+    socket.on('message', (msg, roomname) => {
+        console.log('Message received: ' + msg); // msg는 client에서 보낸 매개변수가 옴
 
-        // io.emit으로 연결된 모든 소켓들에 신호를 보낼 수 있다.
-        io.emit('message', msg);
+        io.to(roomname).emit('message', msg); // 특정 룸의 소켓들에만 신호를 보냄
+    });
+
+    socket.on('joinRoom', (roomname, roomToJoin)=>{
+        socket.leave(roomname); // 기존 룸을 나가고
+        socket.join(roomToJoin); // 들어갈 룸에 join
+
+        socket.emit('roomChanged', roomToJoin); // 룸을 전환하였음을 알림
     });
 });
 
 debug.on('connection', (socket) =>{
 
     socket.on('getRooms', ()=>{
-        console.log(Array.from(io.sockets.adapter.rooms.keys()));
         socket.emit('rooms', Array.from(io.sockets.adapter.rooms.keys()));
     });
 });
